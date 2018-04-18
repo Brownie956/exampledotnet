82210da7124c77aee92db88a87a77b5cf63e2afb
Chris Brown <chris.brown@performancehorizon.com>
2018-04-18 18:39:48 +0100

    Initial commit

---
+++ myFirstDNApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace myFirstDNApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
