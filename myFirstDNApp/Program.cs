aaa46e3c34a02e9a6ffbb4cc3ead4695dac5f458
Chris Brown <chris.brown@performancehorizon.com>
2018-04-18 19:54:55 +0100

    Added small fibonacci number search function

--- myFirstDNApp/Program.cs
+++ myFirstDNApp/Program.cs
@@ -6,8 +6,20 @@ namespace myFirstDNApp
     {
         static void Main(string[] args)
         {
-            string something = "something";
-            Console.WriteLine($"Hello World!{something}");
+            Program program = new Program();
+
+            var fibonacci_numbers = new System.Collections.Generic.List<int> { 1, 1 };
+            int val = program.GetFibonacciNumber(20);
+            Console.WriteLine(val);
+        }
+
+        public int GetFibonacciNumber(int index){
+            var fibonacci_numbers = new System.Collections.Generic.List<int> { 1, 1 };
+            for (int i = 0; i < index; i++){
+                fibonacci_numbers.Add(fibonacci_numbers[i] + fibonacci_numbers[i + 1]);
+            }
+
+            return fibonacci_numbers[fibonacci_numbers.Count - 3];
         }
     }
 }
